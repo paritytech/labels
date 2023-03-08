770f61f1c313d906d43502975527c82cf7bc45b0
Wilfried Kopp <wilfried@parity.io>
2023-03-08 16:16:55 +0100

    Fix CI for the gh-pages

--- scripts/build-doc.sh
+++ scripts/build-doc.sh
@@ -5,7 +5,10 @@ export COMMIT=$(git rev-parse HEAD)
 
 mkdir -p ./docs/src
 
-for chain in polkadot substrate cumulus; do
+chains=$1
+chains=${chains:-"polkadot substrate cumulus"}
+
+for chain in $chains; do
     echo "Generating doc for $chain..."
     tera --include --env --env-key env --template templates/template.md.tera ruled_labels/specs_${chain}.yaml >./docs/src/doc_${chain}.md
 done
